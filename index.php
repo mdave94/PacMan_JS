7738c96df1b4ec75646950ca2a5c9e1d0e8fe589
David Molnár <molnardavid2002@gmail.com>
2018-10-16 14:41:53 +0200

    na ez megy?

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css"  href="jsStyle.css" />
+    <script src="jsJavascript.js"></script>
+    
+</head>
+
+
+<body>
+    <div class="container"  >
+
+        <div id="firstCube"  onclick="changeColor(this)" >
+           
+        </div>
+    
+
+        <div class="secondCube" onclick="changeColor(this)">
+        
+        </div>
+
+
+
+
+    
+    
+    
+    </div>
+
+
+</body>
+</html>
