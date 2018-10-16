d74c80505e3c7ea00c7866bf2f019caefed3bb29
David Molnár <molnardavid2002@gmail.com>
2018-10-16 14:55:14 +0200

    Basic Packman game in JavaScript

---
+++ JsCourseJS.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Game tutorial</title>
+</head>
+<style>
+    canvas {
+        border: solid 1px #000;
+    }
+</style>
+
+<body>
+
+</body>
+<script src="Js.js"> </script>
+</html>
